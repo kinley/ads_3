acc139be106e3d4a07702cdf8de4c1e90aad46c4
Anvar Tuykin <tuykin@gmail.com>
2014-03-19 04:22:43 +0400

    Добавил STDLIB и пример работы с ним

---
+++ _materials/basics/src/Main.java
@@ -0,0 +1,47 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+//		/* STANDART INPUT & OUTOUT */
+//		StdOut.print("hello ");
+//		StdOut.println("world!");
+//		String s = StdIn.readLine();
+//		StdOut.println(s);
+//		int i = StdIn.readInt();
+//		StdOut.println(i);
+		
+//		/* FILE INPUT & OUTPUT */
+//		In in = new In("input.txt");
+//		String data = in.readAll();
+//		StdOut.println(data);
+//		
+//		String filename = "output.txt";
+//		Out out = new Out(filename);
+//        out.println(data);
+//        out.close();
+		
+//		/* DRAWING */
+//		StdDraw.square(.5, .5, .5);
+//		StdDraw.setPenColor(StdDraw.BLUE);
+//		StdDraw.line(.5, .5, .9, .5);
+//		StdDraw.line(.9, .5, .5, .8);
+//		StdDraw.line(.5, .8, .5, .5);
+//		StdDraw.circle(.7, .65, .25);
+		
+//		/* RANDOMIZING */
+//		int N = 10;
+//		for (int i = 0; i < 5; i++)
+//			StdOut.println(StdRandom.uniform(N));
+//		
+//		int[] a = new int[10];
+//		for (int i = 0; i < N; i++)
+//			a[i] = i;
+//		for (int i = 0; i < N; i++)
+//			StdOut.print(a[i] + " ");
+//		StdOut.println();
+//		StdRandom.shuffle(a);
+//		for (int i = 0; i < N; i++)
+//			StdOut.print(a[i] + " ");
+	}
+
+}
