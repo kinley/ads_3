bcd5d12796c0e5ef3ecb0708c69b2827ec587691
Artur <jokidoki@yandex.ru>
2014-05-03 19:23:06 +0400

    Хусаинов переделал 3 задачу

--- 2-lists/husainov_artur/Zadacha3.java
+++ 2-lists/husainov_artur/Zadacha3.java
@@ -1,4 +1,4 @@
-package tuikin_bolel;
+package tuikin;
 
 import java.util.Scanner;
 
@@ -9,58 +9,94 @@ public class Zadacha3 {
         MyList a=new MyList();
         a.generateFromString(in.nextLine());
         a.show();
+        a.deleteReverse();
+        a.show();
     }
-}
-class Element{
-    public String Word;
-    public Element next;
-    Element(String Word)
-    {
-        this.Word=Word;
-        next=null;
-    }
-}
-class MyList {
-    private Element head;
-    MyList(){
-        head=null;
-    }
-    public void show(){
-        Element element=head;
-        System.out.print("Список : ");
-        showRecurtion(element);
-        System.out.println();
-    }
-    private void showRecurtion(Element element){
-        if(element!=null)
-        {
+    private static class MyList {
+        private class Element{
+            public String Word;
+            public Element next;
+            Element(String Word)
+            {
+                this.Word=Word;
+                next=null;
+            }
+        }
+        private Element head;
+        MyList(){
+            head=null;
+        }
+        public void show(){
+            Element element=head;
+            System.out.print("Список : ");
+            showRecurtion(element);
+            System.out.println();
+        }
+        private void showRecurtion(Element element){ //прочему рекурсией? прост)
+            if(element!=null)
+            {
                 System.out.print("\""+element.Word+"\" ");
                 showRecurtion(element.next);
+            }
+        } 
+        public void add(Element newOne){
+            if(head!=null)
+            {
+                lastElement(head).next= newOne;
+            }
+            else
+            {
+                head=newOne;
+            }
         }
-    } 
-    public void add(Element newOne){
-        if(head!=null)
-        {
-            lastElement(head).next= newOne;
+        private Element lastElement(Element element){
+            if(element.next!=null)
+            {
+                return lastElement(element.next);
+            }
+            else return element;
         }
-        else
-        {
-            head=newOne;
-        }
-    }
-    private Element lastElement(Element element){
-        if(element.next!=null)
-        {
-            return lastElement(element.next);
-        }
-        else
-        return element;
-    }
-    public void generateFromString(String Text){
+        public void generateFromString(String Text){
             String[] arr=Text.split("[^a-zA-z]");
             for(String ss: arr)
             {
                 if(!ss.equals("")) this.add(new Element(ss));
             }
+        }
+        private String reverse(String word){
+            String rWord="";
+            for(int i=word.length()-1;i>=0;i--)
+                rWord=rWord.concat(""+word.charAt(i));
+            return rWord;
+        }
+        public void deleteReverse(){
+            Element element=head;
+            while(element!=null){
+                String rWord=reverse(element.Word);
+                boolean a=false;
+                Element element2=element.next;
+                while(element2!=null&&!a){
+                    if(element2.Word.equals(rWord)) {
+                        element.Word="777";  //не хотелось возиться с перемещениями ссылок
+                        element2.Word="777";
+                        a=true;
+                    }
+                    element2=element2.next;
+                }
+                element=element.next;
+            }
+            cleanAfter();
+        }
+        private void cleanAfter(){
+            if(head!=null){
+                Element element=head; 
+                while(element.next!=null){
+                    if(element.next.Word.equals("777")) element.next=element.next.next;
+                    else element=element.next;
+                }
+                if(head.Word.equals("777")) head=head.next; //первый то не проверяли еще
+            }
+        }
     }
 }
+
