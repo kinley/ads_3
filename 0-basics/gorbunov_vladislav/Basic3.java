fcc4ab099781503c0f1c62045fb385820bd417a0
ghostit <hitts@list.ru>
2014-05-29 01:21:06 +0400

    Basics
    Basics

---
+++ 0-basics/gorbunov_vladislav/Basic3.java
@@ -0,0 +1,25 @@
+package apa2;
+
+	import java.io.File;
+	import java.io.FileNotFoundException;
+	import java.io.FileReader;
+	import java.io.UnsupportedEncodingException;
+	import java.util.Scanner;
+	
+	public class Basic3 {
+		private static void read(File file) throws FileNotFoundException{
+			Scanner in = new Scanner(new FileReader(file));
+			while(in.hasNext()){
+			String s = in.next();
+			int n1 = in.nextInt();
+			int n2 = in.nextInt();
+			System.out.printf("%-14s %d %10d %12.3f\n", s, n1, n2, (float)n1/n2);
+			}
+			in.close();
+		}
+	
+		public static void main(String[] args) throws UnsupportedEncodingException, FileNotFoundException{
+			File file = new File("src/file.txt");
+			read(file);
+		}
+	}
