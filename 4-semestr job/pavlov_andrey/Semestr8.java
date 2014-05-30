30b0e4ccdacc0bfc9d991636ff34c6b03b35e079
AndreyPavlov <andreypavlov811@gmail.com>
2014-05-30 09:39:21 +0400

    добавил выходной файл

--- 4-semestr job/pavlov_andrey/Semestr8.java
+++ 4-semestr job/pavlov_andrey/Semestr8.java
@@ -6,7 +6,11 @@
 
 package semestr8;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.LinkedList;
 import java.util.Scanner;
 import java.util.HashSet;
@@ -25,44 +29,57 @@ public class Semestr8 {
     public static void main(String[] args) throws Exception {
         // TODO code application logic here
         
-        File file = new File("input.txt");
+        File fileIn = new File("input.txt");
+        File fileOut = new File("output.txt");
     	Scanner in;
-        in = new Scanner(file);
+        in = new Scanner(fileIn);
         int sz=in.nextInt();
         PQueue T=new PQueue(in);
-        System.out.println(T.size());
-        System.out.println(sz);
-        while(!T.empty()){
-        
-        System.out.println(T.min());
-        T.removeMin();
-        }
         LinkedList result;
         result=MST(T,sz);
-        out(result);
+        out(result,fileOut);
+        System.out.println("done");
     }
 
     private static LinkedList MST(PQueue T, int size) throws Exception {
         LinkedList result = new LinkedList();
         HashSet set=new HashSet();
-        int i=0;
+        Edge min;
+        min=(Edge) T.min();
+        set.add(min.getV());
+        set.add(min.getW());
+        result.add(T.min());
+        T.removeMin();
+        int i=1;
         while(i<size-1){
-             System.out.println(T.min());
-            Edge min=(Edge) T.min();
-            System.out.println(min);
+            min=(Edge) T.min();
             T.removeMin();
-            if(!set.contains(min.getV())||!set.contains(min.getW())){
-                set.add(min.getV());
-                i++;
-                result.add(min);
-            }
+            if(set.contains(min.getV())){
+                if(!set.contains(min.getW())){
+                    set.add(min.getW());
+                    i++;
+                    result.add(min);
+                    }
+                }
+            else
+                if(set.contains(min.getW()))
+                    if(!set.contains(min.getV())){
+                        set.add(min.getV());
+                        i++;
+                        result.add(min);
+                        }
+                    
         }
         return result;
     }
 
-    private static void out(LinkedList result) {
-         while(!result.isEmpty())
-        System.out.print(result.peek()+" ");
+    private static void out(LinkedList result, File file) throws IOException {
+        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(file)));
+         while(!result.isEmpty()){
+            out.print(result.pop()+" ");
+            out.flush();
+         }
+         
     }
     
    
